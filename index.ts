6e16d8f79b70bce57ba87095dc0e694fda1a6aaa
Kwangtae Kim <zabra7946@naver.com>
2022-09-06 07:45:30 +0900

    add formbody plugin

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ export { postgres } from './plugins/postgres';
 export { cookie, cookieOpt } from './plugins/cookie';
 export { cors } from './plugins/cors';
 export { jwt } from './plugins/jwt';
+export { formBody } from './plugins/formbody';
 
 export { BadRequestError } from './errors/400-bad-request';
 export { UnAuthorizedError } from './errors/401-unauthorized';
